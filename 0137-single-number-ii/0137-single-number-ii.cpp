7ae9ce90ece144814453ec85b1f86eb7b8b2c5a8
Nikhil Goel <48389126+nikhil-goel11@users.noreply.github.com>
2025-07-26 14:49:24 +0530

    Time: 0 ms (100%), Space: 13.2 MB (98.5%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ones =0, twos =0;
+        for(int n : nums){
+            ones = (ones ^ n)& ~ twos;
+            twos = (twos ^ n)& ~ ones;
+        }
+        return ones;
+    }
+};
