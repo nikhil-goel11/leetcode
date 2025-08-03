4b850a67c176c5abb52d7951250f36e0f65b65c8
Nikhil Goel <48389126+nikhil-goel11@users.noreply.github.com>
2025-08-03 21:45:24 +0530

    Time: 11 ms (87.13%), Space: 27.7 MB (70.78%) - LeetHub

--- 0218-the-skyline-problem/0218-the-skyline-problem.cpp
+++ 0218-the-skyline-problem/0218-the-skyline-problem.cpp
@@ -3,16 +3,15 @@ class Solution {
         vector<vector<int>> getSkyline(vector<vector<int>>& buildings) {
                 vector<pair<int, int>> events;
                         for (auto& b : buildings) {
-                                    events.emplace_back(b[0], -b[2]); // start, use negative for max-heap
-                                                events.emplace_back(b[1], b[2]);  // end
+                                    events.emplace_back(b[0], -b[2]); 
+                                                events.emplace_back(b[1], b[2]);  
                                                         }
                                                                 sort(events.begin(), events.end());
                                                                         multiset<int> heights = {0};
                                                                                 int prev = 0;
                                                                                         vector<vector<int>> res;
                                                                                                 for (auto& [x, h] : events) {
-                                                                                                            if (h < 0) heights.insert(-h);      // entering building
-                                                                                                                        else heights.erase(heights.find(h)); // leaving building
+                                                                                                            if (h < 0) heights.insert(-h);                                                               else heights.erase(heights.find(h)); 
                                                                                                                                     int curr = *heights.rbegin();
                                                                                                                                                 if (curr != prev) {
                                                                                                                                                                 res.push_back({x, curr});
