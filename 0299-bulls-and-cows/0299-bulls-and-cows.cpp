abaa8b52b803b3c264ff2f7bef3fc6b3b376d331
Nikhil Goel <48389126+nikhil-goel11@users.noreply.github.com>
2025-08-06 21:41:11 +0530

    Time: 3 ms (30.42%), Space: 9 MB (65.91%) - LeetHub

---
+++ 0299-bulls-and-cows/0299-bulls-and-cows.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string getHint(string secret, string guess) {
+        int bulls = 0, cows = 0;
+        vector<int> s_count(10, 0), g_count(10, 0);
+         for (int i = 0; i < secret.size(); ++i) {
+            if (secret[i] == guess[i]) {
+               bulls++; 
+            }
+            else{
+                 s_count[secret[i] - '0']++;
+                 g_count[guess[i] - '0']++;
+            }
+         }
+         for (int i = 0; i < 10; ++i)
+         cows += min(s_count[i], g_count[i]);
+             return to_string(bulls) + "A" + to_string(cows) + "B";
+    }
+};
