2268509d601f041e212a657e9eaca44310c69b38
Nikhil Goel <48389126+nikhil-goel11@users.noreply.github.com>
2025-08-16 11:51:37 +0530

    Time: 0 ms (100%), Space: 8.6 MB (58.55%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -0,0 +1,24 @@
+class Solution {
+    public:
+        int strStr(string haystack, string needle) {
+                int n = haystack.length();
+                        int m = needle.length();
+
+                                if (m > n) return -1;
+
+                                        int i = 0, j = 0;
+
+                                                while (i < n) {
+                                                            if (haystack[i] == needle[j]) {
+                                                                            i++;
+                                                                                            j++;
+                                                                                                            if (j == m) return i - m;
+                                                                                                                        } else {
+                                                                                                                                        i = i - j + 1;
+                                                                                                                                                        j = 0;
+                                                                                                                                                                    }
+                                                                                                                                                                            }
+
+                                                                                                                                                                                    return -1;
+                                                                                                                                                                                        }
+                                                                                                                                                                                        };
