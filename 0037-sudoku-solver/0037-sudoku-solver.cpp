f083d516dac39228e9c2461126869701791a61c9
Nikhil Goel <48389126+nikhil-goel11@users.noreply.github.com>
2025-08-31 14:32:59 +0530

    Time: 380 ms (42.08%), Space: 8.8 MB (27.06%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void solveSudoku(vector<vector<char>>& board) {
+         solve(board);
+             }
+             private:
+                 bool solve(vector<vector<char>>& board) {
+ for (int i = 0; i < 9; ++i) {
+ for (int j = 0; j < 9; ++j) {
+ if (board[i][j] == '.') {
+            for (char c = '1'; c <= '9'; ++c) {
+                                                                                                 if (isValid(board, i, j, c)) {
+ board[i][j] = c;
+   if (solve(board)) return true;
+                                                                                                                                                                                     board[i][j] = '.';
+                                                                                                                                                                                                             }
+                                                                                                                                                                                                                                 }
+                                                                                                                                                                                                 return false;
+                                                                                                                                                                                                                                                                     }
+                                                                                                                                                                                                                                                                                 }
+                                                                                                                                                                                                                                                                                         }
+                                                                                                                                                                                                                                                                                                 return true;
+                                                                                                                                                                                                                                                                                                     }
+bool isValid(vector<vector<char>>& board, int row, int col, char c) {
+    for (int k = 0; k < 9; ++k) {
+                                                                                                                                                                                                                                                                                                                             if (board[row][k] == c) return false; 
+                                                                                                                                                                                                                                                                                                                                         if (board[k][col] == c) return false; 
+                                                                                                                                                                                                                                                                                                                                                     int boxRow = 3 * (row / 3) + k / 3;
+                                                                                                                                                                                                                                                                                                                                                                 int boxCol = 3 * (col / 3) + k % 3;
+ if (board[boxRow][boxCol] == c) return false; 
+                                                                                                                                                                                                                                                                                                                                                                                     }
+   return true; 
+    }
+};
